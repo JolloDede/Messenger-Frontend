340226caa2f2cacf0ebaf4cfe4b131f0060e8be9
JolloDede <jollodede@gmail.com>
2020-12-26 21:37:39 +0100

    Login

--- js/login.js
+++ js/login.js
@@ -3,27 +3,39 @@ const API_URL = "http://localhost:5000/auth/login";
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-  console.log("Hallo");
+  let registered = false;
+  const loginMsg = document.querySelector(".login-message")
 
   const formData = new FormData(form);
   const username = formData.get("username");
   const password = formData.get("password");
 
   const auth = {
-      username: username,
-      password: password
+    username: username,
+    password: password
   };
 
   fetch(API_URL, {
-      method: "POST",
-      body: JSON.stringify(auth),
-      headers: {
-          'content-type': 'application/json'
-      }
+    method: "POST",
+    body: JSON.stringify(auth),
+    headers: {
+      'content-type': 'application/json'
+    }
   }).then(response => {
-    console.log("Hallo");
-    console.log(response);
-    // Todo Write response into cache
+    if (response.ok) {
+      return response.json();
+    }
+    return response.json().then((error) => {
+      throw new Error(error.message);
+    });
+  }).then((result) => {
+    localStorage.token = result.token;
+    registered = true;
+  }).catch((error) => {
+    loginMsg.style.display = "initial";
+  }).then(() => {
+    if (registered) {
+      window.location.href = '../index.html';
+    }
   });
-  window.location.href = '../index.html';
 });
